91bf61b6fafb808664262d2b6c2e52843517e8f2
Desmond McNamee <desmondmcnamee@gmail.com>
2020-04-03 13:25:13 +0200

    Expanded LimitedTable type

--- lib/serverOutbound.d.ts
+++ lib/serverOutbound.d.ts
@@ -1,15 +1,19 @@
 export interface LimitedTable {
+    readonly isStarted: boolean;
     readonly name: string;
+    readonly bettingRound: BettingRound;
+    readonly potChipCount: number;
     readonly seats: Seat[];
-    readonly currentUser: {
-        displayName: string;
-        seatToken: string;
-    };
 }
+export declare type BettingRound = "pre-deal" | "pre-flop" | "flop" | "turn" | "river";
 export interface Seat {
     readonly token: string;
+    readonly isTurn: boolean;
+    readonly chipCount: number;
+    readonly handRoll: HandRoll;
     readonly player?: Player;
 }
+export declare type HandRoll = "dealer" | "small-blind" | "big-blind" | "none";
 export interface Player {
     readonly displayName: string;
 }
