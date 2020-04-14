151fcdab52f2b29464a9357240833fd1e1c333f8
Desmond McNamee <desmondmcnamee@gmail.com>
2020-04-14 20:04:56 +0200

    version 0.1.8

---
+++ scripts/publishVersion.js
@@ -0,0 +1,41 @@
+const { exec } = require("child_process");
+
+const args = process.argv.slice(2);
+const version = args[0];
+
+if (!version) {
+  throw new Error("Version is falsy");
+}
+
+exec(`json -I -f package.json -e 'this.version="${version}"'`, (error) => {
+  if (error) {
+    console.log(`error: ${error.message}`);
+    return;
+  }
+
+  exec(
+    `git add . && git commit -m "version ${version}" && git push && git push origin v${version}`,
+    (error) => {
+      if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+      }
+
+      exec(`git tag -a v${version} -m "version ${version}"`, (error) => {
+        if (error) {
+          console.log(`error: ${error.message}`);
+          return;
+        }
+
+        exec(`git push && git push origin v${version}`, (error) => {
+          if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+          }
+
+          console.log(`🎉 Publish version ${version}`);
+        });
+      });
+    }
+  );
+});
